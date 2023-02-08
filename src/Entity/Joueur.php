e8b566c975bb1a434f77b9af9ec910d142661c1b
jb974 <bertil974@hotmail.fr>
2023-02-08 14:01:29 +0100

    🧸 joueur en bdd

---
+++ src/Entity/Joueur.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JoueurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JoueurRepository::class)]
+class Joueur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $activité = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nb_vaisseau = null;
+
+    #[ORM\Column]
+    private ?bool $alliance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getActivité(): ?string
+    {
+        return $this->activité;
+    }
+
+    public function setActivité(string $activité): self
+    {
+        $this->activité = $activité;
+
+        return $this;
+    }
+
+    public function getNbVaisseau(): ?int
+    {
+        return $this->nb_vaisseau;
+    }
+
+    public function setNbVaisseau(?int $nb_vaisseau): self
+    {
+        $this->nb_vaisseau = $nb_vaisseau;
+
+        return $this;
+    }
+
+    public function isAlliance(): ?bool
+    {
+        return $this->alliance;
+    }
+
+    public function setAlliance(bool $alliance): self
+    {
+        $this->alliance = $alliance;
+
+        return $this;
+    }
+}
